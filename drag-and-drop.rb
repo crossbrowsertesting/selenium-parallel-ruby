cd8b66c4e47ef3d8dfdd76bcaba4c9b299743c85
Luke Kende <luke.kende@gmail.com>
2016-09-15 16:06:18 -0600

    added new examples for form login and drag-and-drop

---
+++ drag-and-drop.rb
@@ -0,0 +1,98 @@
+# Getting started: http://docs.seleniumhq.org/docs/03_webdriver.jsp
+# API details: https://github.com/SeleniumHQ/selenium#selenium
+
+# The gem "Rest-CLient" is highly recommended for making API calls
+# install it with "gem install rest-client"
+
+# For creating unit tests, we recommend test-unit
+# Install it with "gem install test-unit"
+
+require "selenium-webdriver"
+require "rest-client"
+require "test-unit"
+
+class CBT_API
+	@@username = 'you%40yourcompany.com'
+	@@authkey = '12345'
+	@@BaseUrl =   "https://#{@@username}:#{@@authkey}@crossbrowsertesting.com/api/v3"
+	def getSnapshot(sessionId)
+	    # this returns the the snapshot's "hash" which is used in the
+	    # setDescription function
+	    response = RestClient.post(@@BaseUrl + "/selenium/#{sessionId}/snapshots",
+	        "selenium_test_id=#{sessionId}")
+	    snapshotHash = /(?<="hash": ")((\w|\d)*)/.match(response)[0]
+	    return snapshotHash
+	end
+
+	def setDescription(sessionId, snapshotHash, description)
+	    response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}/snapshots/#{snapshotHash}",
+	        "description=#{description}")
+	end
+
+	def setScore(sessionId, score)
+	    # valid scores are 'pass', 'fail', and 'unset'
+	    response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}",
+	        "action=set_score&score=#{score}")
+	end
+end
+
+class DragAndDrop < Test::Unit::TestCase
+	def test_drag_and_drop
+		begin
+			username = "you%40yourcompany.com"
+			authkey = "12345"
+
+			caps = Selenium::WebDriver::Remote::Capabilities.new
+
+			caps["name"] = "Drag and Drop Example"
+			caps["build"] = "1.0"
+			caps["browser_api_name"] = "Chrome53"
+			caps["os_api_name"] = "Win8"
+			caps["screen_resolution"] = "1024x768"
+			caps["record_video"] = "true"
+			caps["record_network"] = "true"
+
+			driver = Selenium::WebDriver.for(:remote,
+			:url => "http://#{username}:#{authkey}@hub.crossbrowsertesting.com:80/wd/hub",
+			:desired_capabilities => caps)
+
+			session_id = driver.session_id
+
+		    score = "pass"
+		    cbt_api = CBT_API.new
+		    # maximize the window - DESKTOPS ONLY
+		    # driver.manage.window.maximize
+
+		    puts "Loading URL"
+		    driver.navigate.to("http://crossbrowsertesting.github.io/drag-and-drop.html")
+
+		    # first, let's grab the draggable element
+		    puts "Grabbing draggable element"
+		    from = driver.find_element(:id, "draggable")
+
+		    # then, we'll grab the droppable element
+		    puts "Grabbing droppable element"
+		    to = driver.find_element(:id, "droppable")
+
+		    # We'll use actions to click and hold the element, drag it, the drop it appropriately
+		    puts "Dragging and dropping element"
+		    driver.action.click_and_hold(from).perform
+		    driver.action.move_to(to).perform
+		    driver.action.release.perform
+
+		    # we'll assert the final state of the droppable element to ensure its in the state we want.
+		    droppableText = driver.find_element(:xpath, "//*[@id=\"droppable\"]/p").text
+		    assert_equal("Dropped!", droppableText)
+
+		    puts "Taking Snapshot"
+		    cbt_api.getSnapshot(session_id)
+		    cbt_api.setScore(session_id, "pass")
+
+		rescue Exception => ex
+		    puts ("#{ex.class}: #{ex.message}")
+		    cbt_api.setScore(session_id, "fail")
+		ensure     
+		    driver.quit
+		end
+	end
+end
---
+++ form-login.rb
@@ -0,0 +1,102 @@
+# Getting started: http://docs.seleniumhq.org/docs/03_webdriver.jsp
+# API details: https://github.com/SeleniumHQ/selenium#selenium
+
+# The gem "Rest-CLient" is highly recommended for making API calls
+# install it with "gem install rest-client"
+
+# For creating unit tests, we recommend test-unit
+# Install it with "gem install test-unit"
+
+require "selenium-webdriver"
+require "rest-client"
+require "test-unit"
+
+class CBT_API
+	@@username = 'you%40yourcompany.com'
+	@@authkey = '12345'
+	@@BaseUrl =   "https://#{@@username}:#{@@authkey}@crossbrowsertesting.com/api/v3"
+	def getSnapshot(sessionId)
+	    # this returns the the snapshot's "hash" which is used in the
+	    # setDescription function
+	    response = RestClient.post(@@BaseUrl + "/selenium/#{sessionId}/snapshots",
+	        "selenium_test_id=#{sessionId}")
+	    snapshotHash = /(?<="hash": ")((\w|\d)*)/.match(response)[0]
+	    return snapshotHash
+	end
+
+	def setDescription(sessionId, snapshotHash, description)
+	    response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}/snapshots/#{snapshotHash}",
+	        "description=#{description}")
+	end
+
+	def setScore(sessionId, score)
+	    # valid scores are 'pass', 'fail', and 'unset'
+	    response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}",
+	        "action=set_score&score=#{score}")
+	end
+end
+
+class FormLogin < Test::Unit::TestCase
+	def test_form_login
+		begin
+			username = "you%40yourcompany.com"
+			authkey = "12345"
+
+			caps = Selenium::WebDriver::Remote::Capabilities.new
+
+			caps["name"] = "Form Login Example"
+			caps["build"] = "1.0"
+			caps["browser_api_name"] = "Chrome53"
+			caps["os_api_name"] = "Win8"
+			caps["screen_resolution"] = "1024x768"
+			caps["record_video"] = "true"
+			caps["record_network"] = "true"
+
+			driver = Selenium::WebDriver.for(:remote,
+			:url => "http://#{username}:#{authkey}@hub.crossbrowsertesting.com:80/wd/hub",
+			:desired_capabilities => caps)
+
+			session_id = driver.session_id
+
+		    score = "pass"
+		    cbt_api = CBT_API.new
+		    # maximize the window - DESKTOPS ONLY
+		    driver.manage.window.maximize
+
+		    puts "Loading URL"
+		    driver.navigate.to("http://crossbrowsertesting.github.io/login-form.html")
+
+		    # start login process by entering username
+		    puts "Entering username"
+		    driver.find_element(:name, "username").send_keys("tester@crossbrowsertesting.com")
+
+		    # then we'll enter the password
+		    puts "Entering password"
+		    driver.find_element(:name, "password").send_keys("test123")
+
+		    # then we'll click the login button
+		    puts "Logging in"
+		    driver.find_element(:css, "div.form-actions > button").click
+
+		    # let's wait here to ensure that the page is fully loaded before we move forward
+		    wait = Selenium::WebDriver::Wait.new(:timout => 10)
+		    wait.until {
+		    	driver.find_element(:xpath, "//*[@id=\"logged-in-message\"]/h2")
+		    }
+
+		    # if we passed the login, then we should see some welcomeText
+		    welcomeText = driver.find_element(:xpath, "//*[@id=\"logged-in-message\"]/h2").text
+		    assert_equal("Welcome tester@crossbrowsertesting.com", welcomeText)
+
+		    puts "Taking Snapshot"
+		    cbt_api.getSnapshot(session_id)
+		    cbt_api.setScore(session_id, "pass")
+
+		rescue Exception => ex
+		    puts ("#{ex.class}: #{ex.message}")
+		    cbt_api.setScore(session_id, "fail")
+		ensure     
+		    driver.quit
+		end
+	end
+end
