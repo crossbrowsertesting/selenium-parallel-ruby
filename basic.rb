63cef19f4fdde0336c9dddb31d0036b8f9bbaf6c
Luke Kende <luke.kende@gmail.com>
2016-09-15 16:02:31 -0600

    updated basic test

--- basic.rb
+++ basic.rb
@@ -4,75 +4,86 @@
 # The gem "Rest-CLient" is highly recommended for making API calls
 # install it with "gem install rest-client"
 
+# For creating unit tests, we recommend test-unit
+# Install it with "gem install test-unit"
+
 require "selenium-webdriver"
 require "rest-client"
+require "test-unit"
 
-# username and authkey go here:
-username = 'user@email.com'
-authkey = '12345'
+class CBT_API
+    @@username = 'you%40yourcompany.com'
+    @@authkey = '12345'
+    @@BaseUrl =   "https://#{@@username}:#{@@authkey}@crossbrowsertesting.com/api/v3"
+    def getSnapshot(sessionId)
+        # this returns the the snapshot's "hash" which is used in the
+        # setDescription function
+        response = RestClient.post(@@BaseUrl + "/selenium/#{sessionId}/snapshots",
+            "selenium_test_id=#{sessionId}")
+        snapshotHash = /(?<="hash": ")((\w|\d)*)/.match(response)[0]
+        return snapshotHash
+    end
 
-BaseUrl =   "https://#{username}:#{authkey}@crossbrowsertesting.com/api/v3"
+    def setDescription(sessionId, snapshotHash, description)
+        response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}/snapshots/#{snapshotHash}",
+            "description=#{description}")
+    end
 
-# we use these functions to access the api
-def getSnapshot(sessionId)
-    # this returns the the snapshot's "hash" which is used in the 
-    # setDescription function
-    response = RestClient.post(BaseUrl + "/selenium/#{sessionId}/snapshots", 
-        "selenium_test_id=#{sessionId}")
-    snapshotHash = /(?<="hash": ")((\w|\d)*)/.match(response)[0]
-    return snapshotHash
+    def setScore(sessionId, score)
+        # valid scores are 'pass', 'fail', and 'unset'
+        response = RestClient.put(@@BaseUrl + "/selenium/#{sessionId}",
+            "action=set_score&score=#{score}")
+    end
 end
 
-def setDescription(sessionId, snapshotHash, description)
-    response = RestClient.put(BaseUrl + "/selenium/#{sessionId}/snapshots/#{snapshotHash}", 
-        "description=#{description}")
+class BasicTest < Test::Unit::TestCase
+    def test_cbt
+        begin
+            
+            username = "you%40yourcompany.com"
+            authkey = "12345"
+
+            caps = Selenium::WebDriver::Remote::Capabilities.new
+
+            caps["name"] = "Basic - Selenium Test Example"
+            caps["build"] = "1.0"
+            caps["browser_api_name"] = "Chrome53"
+            caps["os_api_name"] = "Win8"
+            caps["screen_resolution"] = "1024x768"
+            caps["record_video"] = "true"
+            caps["record_network"] = "true"
+
+            driver = Selenium::WebDriver.for(:remote,
+            :url => "http://#{username}:#{authkey}@hub.crossbrowsertesting.com:80/wd/hub",
+            :desired_capabilities => caps)
+
+            session_id = driver.session_id
+
+            score = "pass"
+            cbt_api = CBT_API.new
+            # maximize the window - DESKTOPS ONLY
+            # driver.manage.window.maximize
+
+            puts "Loading URL"
+            driver.navigate.to("http://crossbrowsertesting.github.io/selenium_example_page.html")
+
+            expected_title = "Selenium Test Example Page"
+
+            puts "Grabbing title"
+            actual_title = driver.title
+
+            # we'll assert that the title is what we want
+            assert_equal(expected_title, actual_title)
+
+            puts "Taking Snapshot"
+            cbt_api.getSnapshot(session_id)
+            cbt_api.setScore(session_id, "pass")
+
+        rescue Exception => ex
+            puts ("#{ex.class}: #{ex.message}")
+            cbt_api.setScore(session_id, "fail")
+        ensure     
+            driver.quit
+        end
+    end
 end
-
-def setScore(sessionId, score)
-    # valid scores are 'pass', 'fail', and 'unset'
-    response = RestClient.put(BaseUrl + "/selenium/#{sessionId}", 
-        "action=set_score&score=#{score}")
-end
-
-caps = Selenium::WebDriver::Remote::Capabilities.new
-
-caps["name"] = "Basic - Selenium Test Example"
-caps["build"] = "1.0"
-caps["browser_api_name"] = "IE10"
-caps["os_api_name"] = "Win7x64-C2"
-caps["screen_resolution"] = "1024x768"
-caps["record_video"] = "true"
-caps["record_network"] = "true"
-
-driver = Selenium::WebDriver.for(:remote, 
-    :url => "http://#{username}:#{authkey}@hub.crossbrowsertesting.com:80/wd/hub",
-    :desired_capabilities => caps)
-
-session_id = driver.session_id
-
-puts session_id
-
-score = "pass"
-
-# maximize the window - DESKTOPS ONLY
-driver.manage.window.maximize
-
-driver.navigate.to("http://crossbrowsertesting.github.io/selenium_example_page.html")
-
-expected_title = "Selenium Test Example Page"
-actual_title = driver.title
-
-if not (actual_title == expected_title)
-    # if the actual title isn't what we expect, set the score to fail
-    puts ("actual_t")
-    score = "fail"
-    message = "Error: #{driver.title} does not equal #{expected_title}" 
-end
-
-setScore(session_id, score)
-if score == "fail"
-    # if the test failed, take a snapshot and log the error
-    snapshotHash = getSnapshot(session_id)
-    setDescription(session_id, snapshotHash, $!.message)
-end
-driver.quit
